cb46380fcff45a9d5202ef07f4fb69fdce90fab6
Abiel Sanchez <abiel02sz@gmail.com>
2024-09-10 00:03:19 -0500

    Update desafios.py

--- python_guía/desafios.py
+++ python_guía/desafios.py
@@ -10,3 +10,14 @@ for i in range(1, 101):
     
     else :print(i)
 
+
+
+# desafio N2
+def palin(word):
+    wordPali = word[::-1]
+    if word == wordPali:
+        print(f"{word} es una palabra palíndroma")
+    else: print(f"{word} no es una palabra palíndroma")
+
+word = input("Introduce una palabra políndroma: ")
+palin(word)
